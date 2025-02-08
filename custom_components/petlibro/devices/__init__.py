47a49ab8d25cd6c752e17bbf6956f6fe51670200
Matthew Ingle <mringle94@gmail.com>
2025-02-08 09:36:38 +0000

    Add granary smart camera feeder

--- custom_components/petlibro/devices/__init__.py
+++ custom_components/petlibro/devices/__init__.py
@@ -2,8 +2,10 @@ from typing import Dict, Type
 from .device import Device
 from .feeders.granary_feeder import GranaryFeeder
 from .feeders.granary_camera_feeder import GranaryCameraFeeder
+from .feeders.granary_smart_camera_feeder import GranarySmartCameraFeeder
 
 product_name_map : Dict[str, Type[Device]] = {
     "Granary Feeder": GranaryFeeder,
-    "Granary Camera Feeder": GranaryCameraFeeder
+    "Granary Camera Feeder": GranaryCameraFeeder,
+    "Granary Smart Camera Feeder": GranarySmartCameraFeeder
 }
